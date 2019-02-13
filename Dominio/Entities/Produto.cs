80b16889c1a13c0957f687d93ea2fb53cc1173ad
Ricardo Santana Ribeiro <ricardo.sribeiro@outlook.com>
2019-02-13 21:25:53 -0200

    Fist commit

---
+++ Dominio/Entities/Produto.cs
@@ -0,0 +1,9 @@
+namespace Dominio.Entities
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
